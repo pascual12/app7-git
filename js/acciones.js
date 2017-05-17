7540c623bcb3b10e934c608296604105b6dcfcf4
pascual12 <saul.sacramento.cbtis16@gmail.com>
2017-05-17 12:45:56 -0500

    1subida

---
+++ js/acciones.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+$(document).ready(function (e) {
+	document.addEventListener("deviceready",function(){
+		
+		$('#izquierda').on("swipeleft",function(){
+			navigator.notification.alert("Desliza a la izquierda",function(){"Aplicacion7","Aceptar"});
+		});
+		
+		$('#derecha').on("swipeleft",function(){
+			navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+				switch(opt)
+				{
+					case 1:
+					navigator.notificacion.beep(2);
+					break;
+					
+					case 1:
+					navigator.notificacion.vibrate(1000);
+					break;
+				}
+			},"Aplicacion7","Beep,Vibrate,Cancelar");
+			
+		});
+	},false);
+        });
